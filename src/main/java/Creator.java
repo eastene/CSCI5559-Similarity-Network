5e3bd045c3247426c3569f9eb6af6b40af6647c0
Evan Stene <eastene@gmail.com>
2017-03-17 17:50:23 -0600

    set up template procedure for filling in later

--- src/main/java/Creator.java
+++ src/main/java/Creator.java
@@ -1,6 +1,7 @@
 /**
- * Created by evanstene on 3/17/17.
+ * Created by Evan Stene on 3/17/17.
  */
+
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
@@ -12,15 +13,15 @@ import org.neo4j.procedure.Name;
 import org.neo4j.procedure.PerformsWrites;
 import org.neo4j.procedure.Procedure;
 
-import static org.neo4j.helpers.collection.MapUtil.stringMap;
-import static org.neo4j.procedure.Procedure.Mode.SCHEMA;
-import static org.neo4j.procedure.Procedure.Mode.WRITE;
+import org.neo4j.procedure.*;
 
 public class Creator {
 
     @Context
     public GraphDatabaseService db;
 
-    @Procedure( name = "snf.create_from_csv", mode = WRITE )
-    public
+    @Procedure( name = "snf.create_from_csv", mode = Mode.WRITE )
+    public void create(String filename){
+        //TODO fill out this method
+    }
 }
