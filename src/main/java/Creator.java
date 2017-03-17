d0656f41add511b3ceff2ef828faeb03d497dd4c
Evan Stene <eastene@gmail.com>
2017-03-17 17:40:52 -0600

    fixed neo4j dependencies

--- src/main/java/Creator.java
+++ src/main/java/Creator.java
@@ -1,5 +1,26 @@
 /**
  * Created by evanstene on 3/17/17.
  */
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexManager;
+import org.neo4j.logging.Log;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.PerformsWrites;
+import org.neo4j.procedure.Procedure;
+
+import static org.neo4j.helpers.collection.MapUtil.stringMap;
+import static org.neo4j.procedure.Procedure.Mode.SCHEMA;
+import static org.neo4j.procedure.Procedure.Mode.WRITE;
+
 public class Creator {
+
+    @Context
+    public GraphDatabaseService db;
+
+    @Procedure( name = "snf.create_from_csv", mode = WRITE )
+    public
 }
