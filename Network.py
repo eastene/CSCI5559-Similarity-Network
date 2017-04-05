ed096aa694fdc5ef7e38dc319ba520878836a3c3
Evan <eastene@gmail.com>
2017-04-04 19:38:02 -0600

    updated distance computation method

--- Network.py
+++ Network.py
@@ -1,6 +1,7 @@
 # Interface for the SNF
 
 import DBConnection, Similarity
+import queue
 
 class Network:
     def __init__(self):
@@ -9,19 +10,32 @@ class Network:
     def __del__(self):
         del self.conn
 
-    def computeDistances(self):
-        # get list of patient ids
+    def computeDistances(self, k=20):
+        '''
+        Compute the initial distances between a node and its nearest k neighbors
+        :param k: number of nearest neighbors to consider (default = 20)
+        :return: 
+        '''
+        # priority queue to hold all distances, then keep only the nearest k
+        neighbor_dist = queue.PriorityQueue()
         print("Computing initial distances...")
+        # get list of patient ids
         nodes = self.conn.getPatientIDList()
-        for id_i in nodes:
-            node_i = self.conn.getPatient(id_i[0])
-            for id_j in nodes:
-                if id_i != id_j:
-                    node_j = self.conn.getPatient(id_j[0])
+        for i in range(len(nodes)):
+            # must use [0] subscript to retrieve Patient_ID from the list of attributes (of len 1)
+            node_i = self.conn.getPatient(nodes[i][0])
+            for j in range(i):
+                    node_j = self.conn.getPatient(nodes[j][0])
                     # using [0] from each node to access property list
                     dist = Similarity.distance(list(node_i.values()), list(node_j.values()))
-                    self.conn.addRelation(id_i[0], id_j[0], dist)
+                    neighbor_dist.put((dist, nodes[j][0]))
+
+            for nn in range(k):
+                # set relations for each of the k nearest neighbors to node i
+                if not neighbor_dist.empty():
+                    dist, n_id = neighbor_dist._get()
+                    self.conn.addRelation(nodes[i][0], n_id, dist)
 
 
 n = Network()
-n.computeDistances()
+n.computeDistances(5)
