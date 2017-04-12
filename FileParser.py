4c7ade8b4cb8e51c811d8e68d12aa4b237037978
Evan Stene <eastene@gmail.com>
2017-04-12 17:36:21 -0600

    changed some variable names

--- FileParser.py
+++ FileParser.py
@@ -15,7 +15,7 @@ class FileParser:
         # close db connection
         del self.conn
 
-    def parseCreatorFile(self, fileName, delimit):
+    def parseCreatorFile(self, fileName, delimiter):
         """
         Parse first data type to create the nodes for the graph
         :param fileName: file from which to read (.csv)
@@ -24,14 +24,13 @@ class FileParser:
         """
         with open(fileName, 'r') as f_in:
             # read csv in Dictionary format
-            reader = csv.DictReader(f_in, delimiter=delimit)
+            reader = csv.DictReader(f_in, delimiter=delimiter)
             # read in each node
             for row in reader:
                 # create the node for the patient with the given attributes
                 self.conn.allocatePatient(row)
 
-
-    def parseNewDataType(self, fileName, delimit):
+    def parseNewDataType(self, fileName, delimiter):
         """
         Parse additional data types for existing nodes in the graph
         :param fileName: file from which to read (.csv)
@@ -40,7 +39,7 @@ class FileParser:
         """
         with open(fileName, 'r') as f_in:
             # read csv in Dictionary format
-            reader = csv.DictReader(f_in, delimiter=delimit)
+            reader = csv.DictReader(f_in, delimiter=delimiter)
             # read in new data type for each node
             for row in reader:
                 # extract patient ID to identify patient, but do not add new id
